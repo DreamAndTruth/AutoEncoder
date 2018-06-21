0a21717c5d93cb0675ba6b6f513b092da3b5e524
Meng Wang <www.13484548124@163.com>
2018-06-22 03:04:49 +0800

    6-22-03:00

--- DenoisingAutoEncoder.py
+++ DenoisingAutoEncoder.py
@@ -23,9 +23,30 @@ class AdditiveGaussianNoiseAutoencoder(object):
         """Initialize the class."""
         self.n_input = n_input
         self.n_hidden = n_hidden
-        self.transfer_function = transfer_function
+        self.transfer = transfer_function
         self.scale = tf.placeholder(tf.float32)
         self.training_scale = scale
         network_weights = self._initialize_weights()
         self.weights = network_weights
         self.x = tf.placeholder(tf.float32, [None, self.n_input])
+        self.hidden = self.transfer(tf.add(
+            tf.matmul(self.x + self.scale*tf.random_normal(self.n_input),
+                      self.weights['w1']), self.weights['b1']))
+        self.reconstruction = tf.add(tf.matmul(self.hidden,
+                                               self.weights['w2']),
+                                     self.weights['b2'])
+        self.cost = tf.reduce_sum(tf.pow(tf.substruct(self.reconstruction,
+                                                      self.x), 2.0))
+        self.optimizer = optimizer(self.cost)
+        self.sess = tf.Session()
+        self.sess.run(tf.global_variables_initializer())
+
+    def _initialize_weights(self):
+        all_weights = {}
+        all_weights['w1'] = tf.Variable(
+            xavier_init(self.n_input, self.n_hidden),
+            dtype=tf.float32)
+        all_weights['w2'] = tf.Variable(
+            xavier_init(self.n_hidden, self.n_input),
+            dtype=tf.float32
+        )
